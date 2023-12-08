cc76d1c8746f76d830deb20a8341bef0c14c1e57
Kiadtiphong Wongmui <122511756+Kiadtiphong307@users.noreply.github.com>
2023-12-08 10:13:20 +0700

    upload files ```

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
  var score = 50
 
+
+    
     //  แบบที่ 1
     //  var exam = "fail"
     // if (score >= 50)  exam = "pass"
@@ -15,6 +17,8 @@ fun main(args: Array<String>) {
     // //     "pass"} else {"fail"}
     // println("The score is $score, result is $exam")
 
+
+    // แบบที่ 1 
 // var grage = "X"
 // if (score >= 80) {grage = "A"}
 //     else if (score >= 70) {grage = "B"}
@@ -22,13 +26,33 @@ fun main(args: Array<String>) {
 //     else if (score >= 50) {grage = "D"}
 //     else {grage = "F"}
 
-var grade = if (score >= 80) "A"
-    else if (score >= 70) "B"
-    else if (score >= 60) "C"
-    else if (score >= 50) "D"
-    else  "F"
+
+// แบบที่ 2
+// var grade = if (score >= 80) "A"
+//     else if (score >= 70) "B"
+//     else if (score >= 60) "C"
+//     else if (score >= 50) "D"
+//     else  "F"
     
-    println ("score = $score, grade = $grade")
+//     println ("score = $score, grade = $grade")
+
+
+// var lang = "dasd"
+// var name = when (lang){
+//     "th"-> "thai"
+//     "en"->  "english"
+//     "jp"-> "japan"
+//     else -> "i don't unknow"
+//     }
+//     println(name)
+
+
+for (a in 1..10 step 2) 
+    println("a = $a") 
+
+
 
 
 }
+
+
